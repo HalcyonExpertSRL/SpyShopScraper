e3a6bd1dd3f046fd6534a23e07d42dd063f38ba1
laasso <nrubialesguerrero@gmail.com>
2024-06-05 11:33:01 +0200

    Refactor scraper.py and saveCSV.py to improve CSV file handling and merging

--- app/scraper.py
+++ app/scraper.py
@@ -4,28 +4,32 @@ import saveCSV
 import htmlget
 URL = "https://www.spy-shop.ro"
 
-'''
-products = saveProducts.getProduct()
+def menu():
+    print("Menu:")
+    print("1. Save to CSv")
+    print("2. Save Products")
+    print("3. Order CSV")
+    print("4. Exit")
 
-#print(htmlText)
+    choose = "3"
+    if choose == "1":
+        requestInstanceCSV = htmlget.Request()
+        csv = saveCSV.CSV()
+        listUrls = requestInstanceCSV.getUrls(URL)
+        for item in listUrls:
+            csv.checkAttr(item)
 
-x = csv.checkAttr(URL)
-csv.mergeLines()
-
-isntance =  getUrls.Urls()
-
-x = isntance.getUrls(URL)
-print(x) 
-'''
-
-requestInstanceCSV = htmlget.Request()
-csv = saveCSV.CSV()
-
-
-listUrls = requestInstanceCSV.getUrls(URL)
-
-for item in listUrls:
-    csv.checkAttr(item)
-
+    elif choose == "2":
+        saveProducts.getProduct()
+    elif choose == "3":
+        csv = saveCSV.CSV()
+        csv.mergeLines()
+    elif choose == "4":
+        print("Exiting...")
+        return
+    else:
+        print("Invalid choose. Please try again.")
 
 
+
+menu()
